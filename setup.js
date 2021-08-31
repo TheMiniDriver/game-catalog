a8aa23d15b67105a7d3edd5fe3fdd36df9855bc0
Bradley Van Aardt <bradley@indri.co.za>
2021-08-31 16:41:36 +0200

    added script to create table

--- setup.js
+++ setup.js
@@ -5,7 +5,22 @@ console.log('Setting up Database....');
 const connection = mysql.createConnection(process.env.DATABASE_URL);
 
 connection.execute('SHOW TABLES;', function(err, results, fields){
-  console.log(err); 
   console.log(results); 
-  console.log(fields); 
+}); 
+
+connection.execute(`CREATE TABLE 'Games's
+    (
+      'id' INT NOT NULL auto_increment,
+      'title' VARCHAR(500) NOT NULL, 
+      'platform' VARCHAR(500) NOT NULL,
+      'year' INT,
+      'publisher' VARCHAR(500) NOT NULL
+    );
+    `, function(err, results, fields){
+  console.log(results); 
+}); 
+
+
+connection.execute('SHOW TABLES;', function(err, results, fields){
+  console.log(results); 
 }); 
